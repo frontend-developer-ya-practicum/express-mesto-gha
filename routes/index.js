651c7bdbeb669b6dcefe51fb9342bf94a946d494
Александр Морев <a.morev@kryptonite.ru>
2022-11-14 02:30:18 +0300

    Refactor joi validation

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,13 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 const auth = require('../middlewares/auth');
-const { REGEX_LINK } = require('../constants/regex');
 const NotFoundError = require('../errors/not-found');
-
+const { validateRegisterBody, validateLoginBody } = require('../middlewares/validation');
 const { register, login } = require('../controllers/auth');
 
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(REGEX_LINK),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), register);
-
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
+router.post('/signup', validateRegisterBody, register);
+router.post('/signin', validateLoginBody, login);
 
 router.use(auth);
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-const { REGEX_LINK } = require('../constants/regex');
+const {
+  validateUserId,
+  validateUserInfoBody,
+  validateUserAvatarBody,
+} = require('../middlewares/validation');
 
 const {
   getCurrentUser,
@@ -12,21 +15,8 @@ const {
 
 router.get('/', getUsers);
 router.get('/me', getCurrentUser);
-router.get('/:userId', celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().required().length(24).hex(),
-  }),
-}), getUser);
-router.patch('/me', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-  }),
-}), updateUserInfo);
-router.patch('/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().required().pattern(REGEX_LINK),
-  }),
-}), updateUserAvatar);
+router.get('/:userId', validateUserId, getUser);
+router.patch('/me', validateUserInfoBody, updateUserInfo);
+router.patch('/me/avatar', validateUserAvatarBody, updateUserAvatar);
 
 module.exports = router;
