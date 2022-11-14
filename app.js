43b0ae8624701157421df4d4e1150d4f0f7b79b5
Александр Морев <a.morev@kryptonite.ru>
2022-11-14 08:32:21 +0300

    Add DB_URI, DEFAULT_PORT constants

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const routes = require('./routes');
+const { DB_URI } = require('./constants/db');
+const { DEFAULT_PORT } = require('./constants/conn');
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-});
+mongoose.connect(DB_URI, { useNewUrlParser: true });
 
-const { PORT = 3000 } = process.env;
+const { PORT = DEFAULT_PORT } = process.env;
 
 const app = express();
 
