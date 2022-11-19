8be391d295330c6363720d28ca3017be8ce89711
Александр Морев <a.morev@kryptonite.ru>
2022-11-19 21:17:28 +0300

    Add request and error loggers

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { errors } = require('celebrate');
 const routes = require('./routes');
 const { DB_URI } = require('./constants/db');
 const { DEFAULT_PORT } = require('./constants/conn');
+const { requestLogger, errorLogger } = require('./middlewares/loggers');
 
 mongoose.connect(DB_URI, { useNewUrlParser: true });
 
@@ -16,7 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.use(routes);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(require('./middlewares/error-handling'));
